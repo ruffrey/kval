dfe79f02a2374a4f04a3180af570ef80064ce994
Jeff Parrish <jeffhparrish@gmail.com>
2015-07-18 20:53:38 -0700

    Clustering on kval startup

--- kval.js
+++ kval.js
@@ -8,9 +8,7 @@ var Db = require('./lib/dbms/dbms');
 function startWorker() {
     process.title = 'kval_worker';
     new Db().initialize(argv, function (err) {
-        if (err) {
-            throw err;
-        }
+        if (err) { throw err; }
     });
 }
 if (cluster.isMaster) {
@@ -21,7 +19,7 @@ if (cluster.isMaster) {
     }
     cluster.on('exit', function (worker, code, signal) {
         console.log('worker ' + worker.process.pid + ' died');
-        startWorker();
+        cluster.fork();
     });
 } else {
     startWorker();
