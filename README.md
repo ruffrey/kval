2452aaa45ded9bf3b27cab5bf2f444833b317e0c
Jeff Parrish <jeffhparrish@gmail.com>
2015-07-20 15:15:58 -0700

    global module and cli

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # kval
 
-A thoughtlessly simplistic and easy to scale database. Bringing scalability
-to the masses.
+A thoughtlessly simplistic and easy to scale database.
+
+## Setup
+
+1. Node.js ^v0.12.5
+    - [nvm](https://github.com/creationix/nvm) is an easy option for installing and managing Node.js versions
+1. Terminal: `npm install -g kval`
+1. Terminal: `kval start`
+    - a full list of options can be seen by running `kval help`
---
+++ worker.js
@@ -0,0 +1,21 @@
+'use strict';
+process.env.DEBUG = 'kval*';
+process.on('uncaughtException', function (err) {
+    console.error('Worker uncaughtException event', err.message, err.stack);
+    process.exit();
+});
+var Db = require('./lib/dbms/dbms');
+
+var env = {
+    host: process.env.KVAL_WORKER_HOST,
+    port: process.env.KVAL_WORKER_POST,
+    path: process.env.KVAL_WORKER_PATH,
+    mapSize: parseFloat(process.env.KVAL_WORKER_MAPSIZE)
+};
+console.log('kval worker about to initialize', env);
+new Db().initialize(env, function (err) {
+    if (err) {
+        console.log('db worker init error', err.message, err.stack);
+        throw err;
+    }
+});
