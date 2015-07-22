6ff37d51c901a49b2b17acd8bf332077f50028cf
Jeff Parrish <jeffhparrish@gmail.com>
2015-07-21 21:39:05 -0700

    fixed kval env db-config

--- worker.js
+++ worker.js
@@ -8,9 +8,9 @@ var Db = require('./lib/dbms/dbms');
 
 var env = {
     host: process.env.KVAL_WORKER_HOST,
-    port: process.env.KVAL_WORKER_POST,
-    path: process.env.KVAL_WORKER_PATH,
-    mapSize: parseFloat(process.env.KVAL_WORKER_MAPSIZE),
+    port: process.env.KVAL_WORKER_PORT,
+    path: process.env.KVAL_WORKER_DB_PATH,
+    mapSize: process.env.KVAL_WORKER_DB_MAX_SIZE_BYTES,
     password: process.env.KVAL_WORKER_PASSWORD
 };
 console.log('kval worker about to initialize', env);
