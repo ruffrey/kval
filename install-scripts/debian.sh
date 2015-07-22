4a33ef7fbe850c7feb9dd38467d8d6f3c470151b
Jeff Parrish <jeffhparrish@gmail.com>
2015-07-22 12:28:13 -0700

    fix debian script sourcing

--- install-scripts/debian.sh
+++ install-scripts/debian.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/env bash
 
-apt-get update
-apt-get install git curl build-essential python2.7 # for building native node modules
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
-source ~/.bashrc
+sudo apt-get update -y
+sudo apt-get install -y git curl build-essential python2.7 # for building native node modules
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash; source ~/.bashrc;
 nvm install node
 node --version
 npm install -g pm2
