6ca7b15202506fcadf44d58b333b8a92f04362cc
Jeff Parrish <jeffhparrish@gmail.com>
2015-07-22 19:38:46 -0700

    test bash debian script

--- install-scripts/debian.sh
+++ install-scripts/debian.sh
@@ -2,9 +2,9 @@
 
 sudo apt-get update -y
 sudo apt-get install -y git curl build-essential python2.7 # for building native node modules
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | NVM_DIR=/usr/local/nvm bash
-source ~/.bashrc
-nvm install node
+sudo mkdir /usr/local/nvm
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | NVM_DIR=/usr/local/nvm bash
+/usr/local/nvm install node
 node --version
 npm install -g pm2
 git clone https://bitbucket.org/ruffrey/kval.git ~/kval
