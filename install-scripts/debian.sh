3d280ad0c1b84a681e9bc5022e4ec5e360ef88e3
Jeff Parrish <jeffhparrish@gmail.com>
2015-07-22 19:46:21 -0700

    debian script wip

--- install-scripts/debian.sh
+++ install-scripts/debian.sh
@@ -1,14 +1,15 @@
 #! /usr/bin/env bash
 
+NODE_V=0.12.7
+
 sudo apt-get update -y
 sudo apt-get install -y git curl build-essential python2.7 # for building native node modules
-sudo mkdir /usr/local/nvm
-sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | NVM_DIR=/usr/local/nvm bash
-/usr/local/nvm install node
-node --version
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install $NODE_V
 npm install -g pm2
 git clone https://bitbucket.org/ruffrey/kval.git ~/kval
 cd kval
 npm i --production
 pm2 start db-config.json
-pm2 startup
+sudo env PATH=$PATH:/home/$USER/.nvm/versions/node/v$NODE_V/bin pm2 startup linux -u $USER
