2cdbc8ed88c3a7c32800c1e9bf0f21fcf5d48d97
Jeff Parrish <jeffhparrish@gmail.com>
2015-07-22 19:36:17 -0700

    fixed debian script again

--- install-scripts/debian.sh
+++ install-scripts/debian.sh
@@ -2,12 +2,13 @@
 
 sudo apt-get update -y
 sudo apt-get install -y git curl build-essential python2.7 # for building native node modules
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash; source ~/.bashrc;
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | NVM_DIR=/usr/local/nvm bash
+source ~/.bashrc
 nvm install node
 node --version
 npm install -g pm2
 git clone https://bitbucket.org/ruffrey/kval.git ~/kval
 cd kval
-npm i
+npm i --production
 pm2 start db-config.json
 pm2 startup
